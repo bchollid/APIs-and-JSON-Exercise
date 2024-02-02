d10e523d7bcd49e9c59fffe6e1b5cdb7af193b2a
bchollid <brandonholliday1@gmail.com>
2024-02-02 15:25:46 -0700

    Finished

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            RonVSKanyeAPI ronVKanye = new RonVSKanyeAPI();
+            for(int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"Kanye: {ronVKanye.GenerateKanyeSentence()}");
+                Console.WriteLine($"Ron: {ronVKanye.GenerateRonSentence()}");
+            }
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+        public string GenerateKanyeSentence()
+        {
+            var kanyeClient = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = kanyeClient.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            return kanyeQuote;
+        }
+
+        public string GenerateRonSentence()
+        {
+            var ronClient = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = ronClient.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            return ronQuote;
+        }
     }
 }
